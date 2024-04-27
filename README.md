70a0e0cec28724c628fd22e450147f0688ad43c4
st4l <152074070+St4lV@users.noreply.github.com>
2024-04-27 18:29:02 +0200

    Fixed GlobalLootModifiers (updated forge)
    Fixed GlobalLootModifiers by updating forge, doesn't work on base forge version of the JDK;
    
    Added coins drop from entities :
    
    - bronze : creeper, zombie, drowned, spider, cave spider, skeleton, pillager, witch, silverfish, zombified piglin, blaze, endermite, guardian ;
    
    - silver : enderman, wither skeleton, vindicator, ghast, piglin, hoglin ;
    
    - gold : shulker, evoker, piglin brute, elder guardian ;
    
    Added coins in structures chests :
    
    - bronze : villages, beached shipwreck ;
    
    - silver : jungle pyramid, desert pyramid, nether fortress, bastion remnants, ruined portal, shipwreck, ancient_city ;
    
    - gold :  buried treasure, end_city , stronghold ;
    
    Added coins in suspicious sand :
    
    - gold : desert_pyramid

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 ### Multiplayer and Economy oriented Minecraft mod;
 
-* **Money :** Bronze, silver, and gold coins ! 100 bronze coins = 1 silver coin; 100 silver coins = 1 gold coin;
+* **Money :** Bronze, silver, and gold coins ! *100 bronze coins = 1 silver coin; 100 silver coins = 1 gold coin;*
 * **Purse :** right click on it to get a new inventory slot allowed to coins !
 * **Blockberg Terminal :** Used to trade items on stock market, can be find in villages in a new house !
+* **New Villager :** The **Stock Trader** ! Profession acquired with **Blockberg Terminal** will compact your coins by the time you get a **Purse** and sell various items from mod.
 
-----------------
+---
 
-If you want to test how the stocks works before configuring it for your server here is a java project in order to:
+*If you want to test how the stocks works before configuring it for your server here is a simple java project in order to:*
 
 https://github.com/St4lV/RPG-Economics-Testing-core-calculs-module
--- src/main/java/fr/st4lV/mcrpgeco/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/fr/st4lV/mcrpgeco/datagen/ModGlobalLootModifiersProvider.java
@@ -18,29 +18,141 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
     @Override
     protected void start() {
 
+
+        //BRONZE COINS
+
+
+            //ENTITIES
+
         add("bronze_coin_from_creeper", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("entities/creeper")).build() }, ModItems.BRONZE_COIN.get()));
 
         add("bronze_coin_from_zombie", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("entities/zombie")).build() }, ModItems.BRONZE_COIN.get()));
 
+        add("bronze_coin_from_drowned", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/drowned")).build() }, ModItems.BRONZE_COIN.get()));
+
         add("bronze_coin_from_spider", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("entities/spider")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_cave_spider", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/cave_spider")).build() }, ModItems.BRONZE_COIN.get()));
 
         add("bronze_coin_from_skeleton", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("entities/skeleton")).build() }, ModItems.BRONZE_COIN.get()));
 
+        add("bronze_coin_from_pillager", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/pillager")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_witch", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/witch")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_silverfish", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/silverfish")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_zombified_piglin", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/zombified_piglin")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_blaze", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/blaze")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_endermite", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/endermite")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_guardian", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/guardian")).build() }, ModItems.BRONZE_COIN.get()));
+
+            //CHESTS
+
+        add("bronze_coin_from_villages", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/village")).build() }, ModItems.BRONZE_COIN.get()));
+
+        add("bronze_coin_from_beached_shipwreck", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/beached_shipwreck")).build() }, ModItems.BRONZE_COIN.get()));
+
+
+        //SILVER COINS
+
+
+            //ENTITIES
+
+        add("silver_coin_from_enderman", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/enderman")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_blaze", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/blaze")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_wither_skeleton", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/wither_skeleton")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_vindicator", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/vindicator")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_ghast", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/ghast")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_piglin", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/piglin")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_hoglin", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/hoglin")).build() }, ModItems.SILVER_COIN.get()));
+
+            //CHESTS
+
         add("silver_coin_from_jungle_temples", new AddItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build() }, ModItems.SILVER_COIN.get()));
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_pyramid")).build() }, ModItems.SILVER_COIN.get()));
 
         add("silver_coin_from_desert_pyramid", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/desert_pyramid")).build() }, ModItems.SILVER_COIN.get()));
 
+        add("silver_coin_from_nether_fortress", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/nether_fortress")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_bastion_remnant", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_remnant")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_ruined_portal", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/ruined_portal")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_shipwreck", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/shipwreck")).build() }, ModItems.SILVER_COIN.get()));
+
+        add("silver_coin_from_ancient_city", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city")).build() }, ModItems.SILVER_COIN.get()));
+
+        //GOLD COINS
+
+
+            //ENTITIES
+
+        add("gold_coin_from_shulker", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/shulker")).build() }, ModItems.GOLD_COIN.get()));
+
+        add("gold_coin_from_evoker", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/evoker")).build() }, ModItems.GOLD_COIN.get()));
+
+        add("gold_coin_from_piglin_brute", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/piglin_brute")).build() }, ModItems.GOLD_COIN.get()));
+
+        add("bronze_coin_from_elder_guardian", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/elder_guardian")).build() }, ModItems.GOLD_COIN.get()));
+
+        //CHESTS
+
+        add("gold_coin_from_buried_treasure", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/buried_treasure")).build() }, ModItems.GOLD_COIN.get()));
+
+        add("gold_coin_from_end_city", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/end_city")).build() }, ModItems.GOLD_COIN.get()));
+
+        add("gold_coin_from_stronghold", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold")).build() }, ModItems.GOLD_COIN.get()));
+
+        //SUS SAND
+
         add("gold_coin_from_suspicious_sand", new AddSusSandItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("archaeology/desert_pyramid")).build() }, ModItems.GOLD_COIN.get()));
-
-
-
 
     }
 }
